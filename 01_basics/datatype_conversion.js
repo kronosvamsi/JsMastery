0507039a1a3d0a34b8e8bc2cfbf48df8f65d6440
Vamsi krishna <55651940+kronosvamsi@users.noreply.github.com>
2024-06-02 18:42:02 +0000

    datatype_conversion

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,31 @@
+//data type conversion
+
+let strNumber="34343";
+
+let numberConv=Number(strNumber);
+
+// console.log(numberConv);
+// console.log(typeof numberConv);
+
+let value=null;
+
+let valConv=Number(value);
+
+// console.log(valConv);
+// console.log(typeof valConv);
+
+
+let undeclared=undefined;
+
+let undecConv=Number(undeclared);
+
+// console.log(undecConv);
+
+// console.log(typeof undecConv);
+
+let alphaNum="33hdf";
+
+let alphaNumConv=Number(alphaNum);
+
+console.log(alphaNumConv);
+
