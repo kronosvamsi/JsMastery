59ff36e02d53d9eaf870802489ebb336694052d1
Vamsi krishna <55651940+kronosvamsi@users.noreply.github.com>
2024-06-02 07:13:48 +0000

    Variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=13890;
+let accountName="Krishna";
+let accountEmail="krish@gmail.com";
+accountCity="Hyderabad";
+
+
+console.log(accountCity);
+// accountName=90890;
+console.log(accountName);
+console.table([accountId,accountName,accountEmail,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+// Datatypes in JS
+
+let state=0;
+
+let bookName="The Code"
+
+let is_available=true
+
+let author;
+
+let rating=null;
+
+// ---Primitive---
+
+// number
+// string
+// boolean
+// BigInt
+// undefined
+// null
+
+// ---Non primitive---
+
+// Object
