e2ce3e869ce1db1dcb6122dd0dd9c82141957c42
Vamsi krishna <55651940+kronosvamsi@users.noreply.github.com>
2024-06-30 03:41:29 +0000

    iife_js

---
+++ 03_basics/02_iife.js
@@ -0,0 +1,26 @@
+//  IIFE - Immediately Invoked function expressions
+
+
+let num=23;
+
+(function ber(num1){
+
+let Num=2+num1;
+console.log(Num)
+}) (4)
+
+function ber(){
+
+    name="krohna"
+    console.log(name)
+}
+
+
+function ber(){
+
+    return {
+        user:"vamsi"
+    }
+}
+
+console.log(ber())
